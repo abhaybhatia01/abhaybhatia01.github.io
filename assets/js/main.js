e950f00ece55b0d5de2462f8090cfc860a26f96c
Abhay Bhatia <abhay1732002@gmail.com>
2022-07-08 02:51:14 +0530

    added prompt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
-/*
-	Big Picture by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+function construction()  {
+	let person = prompt("this site is underconstruction and It is recomended for desktop mode","don't worry click anything");
+  }
+  construction();
+
 
 (function($) {
 
