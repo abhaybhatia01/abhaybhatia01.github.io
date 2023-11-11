932fa1d451a8508a9e49c5c0eacbc6955bc7ce9b
Abhay Bhatia <abhay1732002@gmail.com>
2023-11-11 16:47:22 +0530

    test

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     let myCarousel = document.getElementById('myCarousel');
+//     let pauseButton = document.getElementById('pauseButton');
+//     let playButton = document.getElementById('playButton');
+  
+//     // Start carousel on document load
+//     new bootstrap.Carousel(myCarousel, {
+//       interval: 2000 // Set your desired interval in milliseconds
+//     });
+  
+//     // Pause and play buttons
+//     pauseButton.addEventListener('click', function() {
+//       let carousel = bootstrap.Carousel.getInstance(myCarousel);
+//       carousel.pause();
+//     });
+  
+//     playButton.addEventListener('click', function() {
+//       let carousel = bootstrap.Carousel.getInstance(myCarousel);
+//       carousel.cycle();
+//     });
+//   });
+  
+  
+  
+  
